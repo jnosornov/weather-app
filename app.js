69754401589207cce99308b751ee8955779ee6ac
Johnathan <you@example.com>
2017-12-17 11:43:56 -0500

    fetch longitude and latitude from google geolocation API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20%lombard%20street`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);

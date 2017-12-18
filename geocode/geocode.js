bac042ba8349fd01d477a50c00483a442b5b027a
Johnathan <you@example.com>
2017-12-18 12:03:59 -0500

    debbuged over query limit issue

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -7,11 +7,14 @@ var geocodeAddress = (address, callback) => {
         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
         json: true
     }, (error, response, body) => {
+        console.log(response.statusCode);
+        console.log(body.status);
         if(error) {
             callback('Unable to connect to Google servers');
         } else if(body.status === 'ZERO_RESULTS') {
             callback('Unable to find that address.');
         } else if(body.status === 'OK') {
+            console.log('Im here');
             callback(undefined, {
                 address: body.results[0].formatted_address,
                 latitude: body.results[0].geometry.location.lat,

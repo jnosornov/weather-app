c744431e897a9b23b71fe850d446854e7b3ec62e
Johnathan <you@example.com>
2017-12-18 12:04:28 -0500

    request to Dark Sky API

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var yargs = require('yargs');
+/*var yargs = require('yargs');
 
 const geocode = require('./geocode/geocode.js');
 
@@ -19,6 +19,26 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if(errorMessage) {
         console.log(errorMessage);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log(results);
+        //console.log(JSON.stringify(results, undefined, 2));
+    }
+});
+*/
+
+//ae73721e50eb8124686e6a510c697d04 API key
+
+
+var request = require('request');
+
+request({
+    url:'https://api.darksky.net/forecast/ae73721e50eb8124686e6a510c697d04/39.9396284,-75.18663959999999',
+    json: true
+}, (error, response, body) => {
+    if(error) {
+        console.log('Unable to connect to forecast.io servers');
+    } else if(!error && response.statusCode === 200) {
+        console.log(`Temperature: ${body.currently.temperature}`);
+    } else {
+        console.log('Unable to fetch weather.');
     }
 });
